ea28775f997a5ea7c1bbb176ff177549cd073dee
Joaquin Fernandez <jnfernandez.ojeda@gmail.com>
2023-05-27 18:12:44 -0300

    node import fix

--- crowdfunding/contracts/crowdFundingProject.sol
+++ crowdfunding/contracts/crowdFundingProject.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract CrowdfundingProject is Ownable {
